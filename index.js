4a3a579f5eb6492abca88566def6ad2e8bdb64e7
Alex <floydreme@yandex.ru>
2020-09-14 14:17:24 +0300

    Changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TOKEN = '1231768718:AAGvxd5DKdOiEBXyvfcIYxaKKrteCySUGPs'
+const TOKEN = 'MY_TOKEN'
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(TOKEN)
@@ -247,23 +247,6 @@ _/remove 3_
 ----------------
 
 [Репозиторий](github.com/sashafromlibertalia/ITMO_DeadlineBot) `, { parse_mode: 'Markdown' })
-})
-
-bot.hears(/тоха (.+)/, ({ reply }) => {
-  const answers = [
-    'Пахую ваще на все',
-    'Заебись, узнал номер своей группы',
-    'Настроение скверное, настроение пойти в сабвей',
-    'Нам всем пизда девочки',
-    'Ты заебал уже со своими вопросами',
-    'Ты заебала уже со своими вопросами',
-    'МНЕ ТАК КАЙФОВО ВЫ БЫ ЗНАЛИ РЕБЯТА!!!!',
-    'А где я? Это СПбГУ?',
-    'И тебе говна поесть желаю)',
-    'Курю траву и гоняю шары - карьера явно не на взлете',
-    'Это новый кадилак!!!!!'
-  ]
-  reply(`${answers[Math.floor(Math.random() * answers.length)]}`)
 })
 
 bot.hears('/add', (ctx) => ctx.reply('Ошибка! Где мне взять дату и название предмета?'))
